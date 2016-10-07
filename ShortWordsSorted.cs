6cfa8b68a731cc55ef1036cf56ec7878ee6ba13e
dimo009 <d_chavdarov@hotmail.com>
2016-10-07 14:10:14 +0300

    Add files via upload

---
+++ ShortWordsSorted.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.ShortWordsSorted
+{
+    class ShortWordsSorted
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = ".;:()[]\\/!?, ".ToCharArray();
+
+            var words = Console.ReadLine().ToLower().Split(separators);
+            var result = words.Where(w => w != "").Where(w => w.Length < 5).OrderBy(w => w);
+            var result1 = result.Distinct();
+
+            Console.WriteLine(string.Join(", ",result1));
+        }
+    }
+}
