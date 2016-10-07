6b66a70bbe62825fc8a8bf28d068765027275487
dimo009 <d_chavdarov@hotmail.com>
2016-10-07 10:45:52 +0300

    Add files via upload

---
+++ ReadOddOccurencesSecond.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Test
+{
+    class ReadOddOccurencesSecond
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToLower();
+            var words = text.Split(' ');
+            Console.WriteLine(String.Join(", ",
+            words.Where(x => words.Where(y => y == x).Count() % 2 == 1).Distinct()));
+        }
+    }
+}
