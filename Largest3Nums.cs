741954cf0a427160a3c392ff145bab3a665767ae
dimo009 <d_chavdarov@hotmail.com>
2016-10-07 12:12:35 +0300

    Add files via upload

---
+++ Largest3Nums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Largest_3_numbers
+{
+    class Largest3Nums
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse);
+            var sorted = nums.OrderByDescending(x => x).Take(3);
+            var biggestThree = sorted.Take(3);
+            Console.WriteLine(String.Join(" ", biggestThree));
+        }
+    }
+}
